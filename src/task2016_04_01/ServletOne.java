1a65b7dc88b8c12babacfd86386655aa38214cdf
st032334 <a.a.a.2711@mail.ru>
2016-04-01 12:12:07 +0300

    init

--- src/task2016_04_01/ServletOne.java
+++ src/task2016_04_01/ServletOne.java
@@ -1,13 +1,16 @@
 package task2016_04_01;
 
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.BufferedWriter;
 import java.io.IOException;
 
 public class ServletOne extends HttpServlet {
     private int i = 0;
-    public void doGet(HttpServletRequest request,HttpServletResponse response) throws IOException {
-        BufferedWriter writer = new BufferedWriter(response.getWriter);
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        BufferedWriter writer = new BufferedWriter(response.getWriter());
         i++;
         writer.write(i);
-        writer.flush;}
+        writer.flush();}
 }
